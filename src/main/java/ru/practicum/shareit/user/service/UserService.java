8d93f7b939423470fc22c626a73b6486f778af30
Shtainbook <shtainbook@yandex.ru>
2023-03-26 00:22:55 +0300

    third step

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -6,9 +6,13 @@ import ru.practicum.shareit.user.dto.UserDto;
 import java.util.List;
 
 public interface UserService {
-    ResponseEntity<UserDto> create (UserDto userDto);
-    ResponseEntity<UserDto>  read (Long id);
-    ResponseEntity<List <UserDto>> readAll ();
-    ResponseEntity<UserDto>  update (UserDto userDto, Long id);
-    ResponseEntity<UserDto>  delete (Long id);
+    ResponseEntity<UserDto> create(UserDto userDto);
+
+    ResponseEntity<UserDto> read(Long id);
+
+    ResponseEntity<List<UserDto>> readAll();
+
+    ResponseEntity<UserDto> update(UserDto userDto, Long id);
+
+    ResponseEntity<UserDto> delete(Long id);
 }
