0583f5a0aec624c929fda9bdc2ceebb6da874bdb
Shtainbook <shtainbook@yandex.ru>
2023-06-09 21:42:07 +0300

    удалил пробел

--- gateway/src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
